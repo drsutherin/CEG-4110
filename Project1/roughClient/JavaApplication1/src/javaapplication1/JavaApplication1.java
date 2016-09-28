a4085809bf089a69faaa1ef83c1e365e8bcd4c06
Michael Rutkowski <mchlrtkwski@gmail.com>
2016-09-28 00:59:43 -0400

    Fixed for Localhost use for Project Specifications

--- Project1/roughClient/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Project1/roughClient/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Documentation was found on the Oracle Website for Socket help
+//http://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
 package javaapplication1;
 
 import java.io.BufferedReader;
@@ -10,45 +7,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class JavaApplication1 {
 
     public static void main(String[] args) throws IOException {
 
-        String serverHostname = new String("24.166.20.116");
+        //Server Name input
+        String server = new String("localhost");
 
-        if (args.length > 0) {
-            serverHostname = args[0];
-        }
-        System.out.println("Attemping to connect to host "
-                + serverHostname + " on port 45322.");
+        System.out.println("Connecting to host "+ server + " on port 45322.");
 
-        Socket socketTest = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
+        //Declare Variables
+        Socket socketToTry = null;
+        PrintWriter socketOut = null;
+        BufferedReader socketIn = null;
 
         try {
-            // echoSocket = new Socket("taranis", 7);
-            socketTest = new Socket(serverHostname, 45322);
-            out = new PrintWriter(socketTest.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(
-                    socketTest.getInputStream()));
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + serverHostname);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for "
-                    + "the connection to: " + serverHostname);
-            System.exit(1);
+            //Create socket
+            socketToTry = new Socket(server, 45322);
+            //Create Writer to write socker output
+            socketOut = new PrintWriter(socketToTry.getOutputStream(), true);
+            //Create Reader to read socket input
+            socketIn = new BufferedReader(new InputStreamReader(socketToTry.getInputStream()));
+
+            //Catch Exceptions relating to failed Connection
+        } catch (Exception e) {
+            System.err.println("Error");
         }
 
-        BufferedReader stdIn = new BufferedReader(
-                new InputStreamReader(System.in));
+        //Read in user input
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        
+        //Read in user input continuously. Once the user has typed something they recieve
+        //feeback from the server. This creates a problem, as the server should relay
+        //messages regardless of if user enters input or not. This is a multithreading 
+        //issue that will be worked on.
         String userInput;
         while ((userInput = stdIn.readLine()) != null) {
-            out.println(userInput);
-            System.out.println("echo: " + in.readLine());
+            socketOut.println(userInput);
+            System.out.println("echo: " + socketIn.readLine());
         }
     }
 }
